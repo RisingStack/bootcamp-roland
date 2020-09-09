356f7b34e980a079746918d211b345a366856695
Kóczán Roland <roland.koczan@risingstack.com>
2020-09-09 12:14:12 +0200

    Removed unused "next" callback

--- router.js
+++ router.js
@@ -1,6 +1,6 @@
 const Router = require('koa-router');
 const router = new Router();
 
-router.get('/hello', (ctx, next) => ctx.body = 'Hello World !');
+router.get('/hello', (ctx) => ctx.body = 'Hello World !');
 
 module.exports = router;
