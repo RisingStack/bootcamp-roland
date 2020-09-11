966e2c4f3acba0f2bbc87f944491ca2565f66d2b
Kóczán Roland <roland.koczan@risingstack.com>
2020-09-11 14:23:03 +0200

    nock intercepts request

--- test/test.js
+++ test/test.js
@@ -8,6 +8,10 @@ const should = chai.should();
 
 chai.use(chaiHttp);
 
+const nock = require('nock');
+
+const scope = nock('https://api.github.com');
+
 describe('GET /hello', () => {
     it('returns \'Hello World ! \'', async () => {
         const response = await chai.request(server).get('/hello');
@@ -18,6 +22,20 @@ describe('GET /hello', () => {
 
 describe('Invoking searchRepositories', () => {
     it('should return something', async () => {
+        scope.post('/graphql').reply(200,{
+            data: {
+                createdAt: '2020',
+                collaborators: {
+                    totalCount: 1,
+                    edges: [{
+                        node: {
+                            login: 'bela'
+                        }
+                    }
+                    ]
+                }
+            }
+        });
         const response = await github.searchRepositories('WordsMemorizer');
         console.log(JSON.stringify(response, undefined, '-'));
     });
@@ -25,6 +43,22 @@ describe('Invoking searchRepositories', () => {
 
 describe('Invoking getContributors', () => {
     it('should return something', async () => {
+        scope.post('/graphql').reply(200,{
+            data: {
+                repository: {
+                    collaborators: {
+                        edges: [{
+                            node: {
+                                id: 'TEST_ID',
+                                login: 'testLogin',
+                                url: 'https://testurl.test',
+                                avatarUrls: 'valami url az avatarhoz'
+                            }
+                        }]
+                    }
+                }
+            }
+        });
         const response = await github.getContributors('RisingStack', 'risingstack-bootcamp-v2');
         console.log(JSON.stringify(response, undefined, '-'));
     });
