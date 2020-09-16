af13f6a04cd1c87ee776ed488deb077484f50e42
Kóczán Roland <roland.koczan@risingstack.com>
2020-09-16 15:36:43 +0200

    Auto format

--- test/test.js
+++ test/test.js
@@ -43,75 +43,75 @@ const getContributorsQueryString = `query collaboratorsQuery($owner:String!,$rep
   }`;
 
 describe('Github service', () => {
-    const githubAPIMock = nock('https://api.github.com');
+  const githubAPIMock = nock('https://api.github.com');
 
-    it('should use Authorization header and a token provided from config.js', async () => {
-        const scope = githubAPIMock.matchHeader('Authorization', `Bearer ${config.githubToken}`).post('/graphql').reply(200, {data: {}});
-        await github.searchRepositories('WordsMemorizer');
-        scope.done();
-    });
+  it('should use Authorization header and a token provided from config.js', async () => {
+    const scope = githubAPIMock.matchHeader('Authorization', `Bearer ${config.githubToken}`).post('/graphql').reply(200, { data: {} });
+    await github.searchRepositories('WordsMemorizer');
+    scope.done();
+  });
 
-    describe('Invoking getContributors', () => {
-        const mockResponse = {
-            repository: {
-                collaborators: {
-                    edges: [{
-                        node: {
-                            id: 'TEST_ID',
-                            login: 'testLogin',
-                            url: 'https://testurl.test',
-                            avatarUrls: 'valami url az avatarhoz'
-                        }
-                    }]
-                }
+  describe('Invoking getContributors', () => {
+    const mockResponse = {
+      repository: {
+        collaborators: {
+          edges: [{
+            node: {
+              id: 'TEST_ID',
+              login: 'testLogin',
+              url: 'https://testurl.test',
+              avatarUrls: 'valami url az avatarhoz'
             }
-        };
+          }]
+        }
+      }
+    };
 
-        it('should return dummy response', async () => {
-            const scope = githubAPIMock.post('/graphql').reply(200, { data: mockResponse });
-            const response = await github.getContributors('RisingStack', 'risingstack-bootcamp-v2');
-            response.should.deep.equal(mockResponse);
-        });
-
-        it('should include "$owner" and "$repoName" query variable', async () => {
-            const scope = githubAPIMock.post('/graphql', (body) => (body.query && body.query === getContributorsQueryString)).reply(200, { data: mockResponse });
-            await github.getContributors('WordsMemorizer');
-            scope.done();
-        });
+    it('should return dummy response', async () => {
+      const scope = githubAPIMock.post('/graphql').reply(200, { data: mockResponse });
+      const response = await github.getContributors('RisingStack', 'risingstack-bootcamp-v2');
+      response.should.deep.equal(mockResponse);
     });
 
-
-    describe('Invoking searchRepositories', () => {
-        const mockResponse = {
-            createdAt: '2020',
-            collaborators: {
-                totalCount: 1,
-                edges: [{ node: { login: 'bela' } }]
-            }
-        };
-
-        it('should return dummy response', async () => {
-            githubAPIMock.post('/graphql').reply(200, { data: mockResponse });
-            const response = await github.searchRepositories('WordsMemorizer');
-            response.should.deep.equal(mockResponse);
-        });
-
-        // TODO: refact
-        it.skip('should include "$queryString" query variable', async () => {
-            const scope = githubAPIMock.post('/graphql', body => (body.query && body.query === searchRepositoryQueryString)).reply(200, {data: mockResponse});
-            await github.searchRepositories('WordsMemorizer');
-            scope.done();
-        });
+    it('should include "$owner" and "$repoName" query variable', async () => {
+      const scope = githubAPIMock.post('/graphql', (body) => (body.query && body.query === getContributorsQueryString)).reply(200, { data: mockResponse });
+      await github.getContributors('WordsMemorizer');
+      scope.done();
     });
+  });
+
+
+  describe('Invoking searchRepositories', () => {
+    const mockResponse = {
+      createdAt: '2020',
+      collaborators: {
+        totalCount: 1,
+        edges: [{ node: { login: 'bela' } }]
+      }
+    };
+
+    it('should return dummy response', async () => {
+      githubAPIMock.post('/graphql').reply(200, { data: mockResponse });
+      const response = await github.searchRepositories('WordsMemorizer');
+      response.should.deep.equal(mockResponse);
+    });
+
+    // TODO: refact
+    it.skip('should include "$queryString" query variable', async () => {
+      const scope = githubAPIMock.post('/graphql', body => (body.query && body.query === searchRepositoryQueryString)).reply(200, { data: mockResponse });
+      await github.searchRepositories('WordsMemorizer');
+      scope.done();
+    });
+  });
 });
 
 describe('Web instance', () => {
-    describe('GET /hello', () => {
-        it('returns \'Hello World ! \'', async () => {
-            const response = await chai.request(server).get('/hello');
-            response.should.have.status(200);
-            response.text.should.equal('Hello World !');
-        });
+  describe('GET /hello', () => {
+    it('returns \'Hello World ! \'', async () => {
+      const response = await chai.request(server).get('/hello');
+      response.should.have.status(200);
+      response.text.should.equal('Hello World !');
     });
+  });
 });
 
