56814abdc81bc7f990fdc9fb4415256d11b1f2d7
rolandKoczan <70887944+rolandKoczan@users.noreply.github.com>
2020-09-18 11:47:07 +0200

    Initialize database for users and repos
    Installed packages required for the DB
    Added DB related configs
    Added DB migration related files
    Finished migration files
    Added migrate-db script
    Changed tableName to strings
    Added migration config to knex object
    Added primary and secondary key creations
    Current DB is reset befor applying latest miration
    Changed IDs to auto increment
    Removed table deletions from migrate-db

---
+++ db/db.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+const config = require('../config');
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        host: config.dbHost,
+        user: config.dbUser,
+        password: config.dbPassword,
+        database: config.db,
+    },
+    migrations: {
+        directory: path.join(__dirname, './migrations')
+    }
+});
+
+module.exports = knex;
