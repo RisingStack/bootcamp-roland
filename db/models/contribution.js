b89221c2f911abbc97b8c3bac5b1f8a9e8692fdc
Kóczán Roland <roland.koczan@risingstack.com>
2020-09-22 16:50:19 +0200

    insertOrReplace query changes

--- db/models/contribution.js
+++ db/models/contribution.js
@@ -22,20 +22,18 @@ async function insert(data) {
 
 async function insertOrReplace({repository, user, line_count}){
     Joi.assert({repository, user, line_count}, Contribution);
-    const test = ['bela', 'janos', 'gyuri'];
-    console.log(...test);
     const response = await db.raw(`
         INSERT INTO contribution ("user",repository,line_count) 
         VALUES (${user}, ${repository}, ${line_count})
-        ON CONFLICT ("user") DO UPDATE SET "user"=EXCLUDED."user", repository=EXCLUDED.repository, line_count=EXCLUDED.line_count
+        ON CONFLICT ("user", repository) DO UPDATE SET "user"=EXCLUDED."user", repository=EXCLUDED.repository, line_count=EXCLUDED.line_count
     `);
     return response;
 }
 
-/*async function read({user: {userId, login}, repository: {repositoryId, full_name}}){
+async function read({user: {id: userID, login}, repository: {id: repositoryId, full_name}}){
     const tables = [];
     if(user){}
-}*/
+}
 
 module.exports = {
     insert,
