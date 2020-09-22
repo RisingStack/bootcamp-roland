0d002eea7bdd0da6f7cc724dfd098835fcfe4d71
Kóczán Roland <roland.koczan@risingstack.com>
2020-09-22 16:10:51 +0200

    Added: insert, insertOrReplace

---
+++ db/models/contribution.js
@@ -0,0 +1,43 @@
+const Joi = require('joi');
+const db = require('../db');
+
+const Contribution = Joi.object({
+    user: Joi.number().required(),
+    repository: Joi.number().required(),
+    line_count: Joi.number().required(),
+});
+
+async function insert(data) {
+    console.log(data);
+    
+    try {
+        Joi.assert(data, Repository);
+        const response = await db('contribution').insert(data);
+        return response;
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+}
+
+async function insertOrReplace({repository, user, line_count}){
+    Joi.assert({repository, user, line_count}, Contribution);
+    const test = ['bela', 'janos', 'gyuri'];
+    console.log(...test);
+    const response = await db.raw(`
+        INSERT INTO contribution ("user",repository,line_count) 
+        VALUES (${user}, ${repository}, ${line_count})
+        ON CONFLICT ("user") DO UPDATE SET "user"=EXCLUDED."user", repository=EXCLUDED.repository, line_count=EXCLUDED.line_count
+    `);
+    return response;
+}
+
+/*async function read({user: {userId, login}, repository: {repositoryId, full_name}}){
+    const tables = [];
+    if(user){}
+}*/
+
+module.exports = {
+    insert,
+    insertOrReplace,
+};
