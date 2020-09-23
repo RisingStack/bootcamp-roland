9081b201b72a2d554ddbe74107fa7123df0378e1
Kóczán Roland <roland.koczan@risingstack.com>
2020-09-23 13:30:38 +0200

    Contribution.read helper funtion

--- db/models/contribution.js
+++ db/models/contribution.js
@@ -9,7 +9,7 @@ const Contribution = Joi.object({
 
 async function insert(data) {
     console.log(data);
-    
+
     try {
         Joi.assert(data, Repository);
         const response = await db('contribution').insert(data);
@@ -20,8 +20,8 @@ async function insert(data) {
     }
 }
 
-async function insertOrReplace({repository, user, line_count}){
-    Joi.assert({repository, user, line_count}, Contribution);
+async function insertOrReplace({ repository, user, line_count }) {
+    Joi.assert({ repository, user, line_count }, Contribution);
     const response = await db.raw(`
         INSERT INTO contribution ("user",repository,line_count) 
         VALUES (${user}, ${repository}, ${line_count})
@@ -30,12 +30,54 @@ async function insertOrReplace({repository, user, line_count}){
     return response;
 }
 
-async function read({user: {id: userID, login}, repository: {id: repositoryId, full_name}}){
-    const tables = [];
-    if(user){}
+async function read({ user = {id: '', login: ''}, repository = {id: '', full_name: ''}}) {
+    let condition = '';
+    let columns = '';
+    console.log(user);
+    console.log(repository);
+
+    console.log(user !== {id: '', login: ''});
+    console.log(repository == {id: '', full_name: ''});
+
+    // If only user provided -> list contributions created by the given user
+    // WHERE "user".id = userID OR "user".login = login
+    // Columns: contribution.user, contribution.repository, line_count, full_name
+    if (user.id !== '' && user.login !== '') {
+        condition = `"user".id = ${user.id} OR "user".login = '${user.login}'`;
+        columns = 'contribution.user, contribution.repository, line_count, full_name';
+    };
+
+    // IF only the repository provided -> list all contributions made to the given repository
+    // WHERE repository.id = repositoryID OR repository.full_name = full_name
+    // Columns: "user".id, "user".login, line_count
+    if (repository.id !== '' && repository.full_name !== '') {
+        condition = `repository.id = ${repository.id} OR full_name = ${repository.full_name}`;
+        columns = '"user".id, login, line_count';
+    };
+
+    // If both params provided -> list all contributions by a user to a single repository
+    // WHERE "user".id = userID OR "user".login = login AND repository.id = repositoryID OR repository.full_name = full_name
+    // Columns: line_count, full_name, login
+    /*if ((repository !== {id: '', login: ''}) && (user !== {id: '', full_name: ''})) {
+        condition = `
+            (repository.id = ${repository.id} OR "user".login = ${repository.full_name}) AND
+            ("user".id = ${user.id} OR login = ${user.login})
+        `;
+        columns = 'line_count, full_name, login';
+    };*/
+
+    const both = await db.raw(`
+        SELECT ${columns} FROM contribution
+        LEFT JOIN "user" ON contribution."user" = "user".id
+        LEFT JOIN repository ON contribution.repository = repository.id
+        WHERE ${condition}
+    `);
+
+    return both;
 }
 
 module.exports = {
     insert,
     insertOrReplace,
+    read
 };
