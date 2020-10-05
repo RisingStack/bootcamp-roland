69c87cfb478edb0be1a9cec7297a4d8e860d7e16
Kóczán Roland <roland.koczan@risingstack.com>
2020-10-05 15:49:35 +0200

    Added tests for /contribution endpoint

--- router.spec.js
+++ router.spec.js
@@ -13,7 +13,37 @@ const should = chai.should();
 
 chai.use(chaiHttp);
 
+const mockRepo = {
+  id: 1,
+  owner: 1,
+  full_name: 'Bela Todo list',
+  description: 'Bela Todo list',
+  html_url: 'https://github.com/',
+  language: 'eng',
+  stargazers_count: 5
+};
+
+const mockUser = {
+  id: 1,
+  login: 'bela',
+  avatar_url: 'https://github.com/avatar',
+  html_url: 'https://github.com/avatar'
+};
+
+const mockContribution = {
+  user: 1,
+  repository: 1,
+  line_count: 50
+};
+
 describe('Web instance', () => {
+  beforeEach(async () => {
+    await knex.raw('TRUNCATE TABLE contribution CASCADE');
+    await knex.raw('TRUNCATE TABLE repository CASCADE');
+    await knex.raw('TRUNCATE TABLE "user" CASCADE');
+    await knex.raw('ALTER SEQUENCE repository_id_seq RESTART WITH 1');
+  });
+
   describe('GET /hello', () => {
     it('returns \'Hello World ! \'', async () => {
       const response = await chai.request(server).get('/hello');
@@ -23,41 +53,19 @@ describe('Web instance', () => {
   });
 
   describe('GET /repository', async () => {
-    beforeEach(async () => {
-      await knex.raw('TRUNCATE TABLE repository CASCADE');
-      await knex.raw('ALTER SEQUENCE repository_id_seq RESTART WITH 1');
-    });
 
     describe('/:id', () => {
       it('returns a repository with given ID', async () => {
-        const dummyRepo = {
-          id: 1,
-          owner: 1,
-          full_name: 'Bela Todo list',
-          description: 'Bela Todo list',
-          html_url: 'https://github.com/',
-          language: 'eng',
-          stargazers_count: 5
-        };
-        await repository.insert(dummyRepo);
-        const response = await chai.request(server).get(`/repository/${dummyRepo.id}`);
+        await repository.insert(mockRepo);
+        const response = await chai.request(server).get(`/repository/${mockRepo.id}`);
         response.should.have.status(200);
-        response.body[0].should.deep.equal(dummyRepo);
+        response.body[0].should.deep.equal(mockRepo);
       });
     });
 
     describe('/:id with invalid path variable', () => {
       it('returns 403', async () => {
-        const dummyRepo = {
-          id: 1,
-          owner: 1,
-          full_name: 'Bela Todo list',
-          description: 'Bela Todo list',
-          html_url: 'https://github.com/',
-          language: 'eng',
-          stargazers_count: 5
-        };
-        await repository.insert(dummyRepo);
+        await repository.insert(mockRepo);
         const response = await chai.request(server).get('/repository/asd');
         response.should.have.status(403);
       });
@@ -65,19 +73,10 @@ describe('Web instance', () => {
 
     describe('?id=', () => {
       it('returns a repository with given ID', async () => {
-        const dummyRepo = {
-          id: 1,
-          owner: 1,
-          full_name: 'Bela Todo list',
-          description: 'Bela Todo list',
-          html_url: 'https://github.com/',
-          language: 'eng',
-          stargazers_count: 5
-        };
-        await repository.insert(dummyRepo);
-        const response = await chai.request(server).get(`/repository?id=${dummyRepo.id}`);
+        await repository.insert(mockRepo);
+        const response = await chai.request(server).get(`/repository?id=${mockRepo.id}`);
         response.should.have.status(200);
-        response.body[0].should.deep.equal(dummyRepo);
+        response.body[0].should.deep.equal(mockRepo);
       });
     });
     describe('?id= with invalid query variable', () => {
@@ -86,33 +85,57 @@ describe('Web instance', () => {
         response.should.have.status(403);
       });
     });
+  });
+  
+  describe('POST /repository', () => {
+    it('returns the inserted object', async () => {
+      const { id, owner, full_name, description, html_url, language, stargazers_count } = mockRepo;
+      await chai.request(server)
+        .post('/repository')
+        .type('application/json')
+        .send({ owner, full_name, description, html_url, language, stargazers_count });
+      const repo = await repository.read({ id });
+      repo[0].should.deep.equal(mockRepo);
+    });
+    it('returns 403', async () => {
+      const response = await chai.request(server)
+        .post('/repository')
+        .type('application/json')
+        .send(mockRepo);
+      response.should.have.status(403);
+    });
+  });
 
-    describe('POST /repository', () => {
-      it('returns the inserted object', async () => {
-        const dummyRepo = {
-          owner: 1,
-          full_name: 'Bela Todo list',
-          description: 'Bela Todo list',
-          html_url: 'https://github.com/',
-          language: 'eng',
-          stargazers_count: 5
-        };
-        await chai.request(server)
-          .post('/repository')
-          .type('application/json')
-          .send(dummyRepo);
-        dummyRepo.id = 1;
-        const repo = await repository.read(dummyRepo);
-        repo[0].should.deep.equal(dummyRepo);
-      });
-      it('returns 403', async () => {
-        const dummyRepo = {};
-        const response = await chai.request(server)
-          .post('/repository')
-          .type('application/json')
-          .send(dummyRepo);
-        response.should.have.status(403);
-      });
+  describe('GET /contribution', () => {
+    it('returns all users contributed to a single repository', async () => {
+      await user.insert(mockUser);
+      await repository.insert(mockRepo);
+      await contribution.insert(mockContribution);
+
+      const response = await chai.request(server).get(`/contribution?userID=${mockUser.id}`);
+      const { user: userResp, repository: repositoryResp } = response.body[0];
+
+      userResp.should.deep.equal({ id: mockUser.id, login: mockUser.login });
+      repositoryResp.should.deep.equal({ id: mockRepo.id, full_name: mockRepo.full_name });
+    });
+  });
+
+  describe('POST /contribution', () => {
+    it('returns inserted contribution', async () => {
+      await user.insert(mockUser);
+      await repository.insert(mockRepo);
+
+      await chai.request(server)
+        .post('/contribution')
+        .type('application/json')
+        .send(mockContribution);
+
+      const response = await contribution.read({ id: mockUser.id });
+      const { user: userResp, repository: repositoryResp } = response[0];
+
+      userResp.should.deep.equal({ id: mockUser.id, login: mockUser.login });
+      repositoryResp.should.deep.equal({ id: mockRepo.id, full_name: mockRepo.full_name });
+
     });
   });
 });
