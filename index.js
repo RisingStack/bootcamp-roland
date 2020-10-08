1bf0fb3bb7e05776d8301e352f068f1880c75145
Kóczán Roland <roland.koczan@risingstack.com>
2020-10-08 11:47:35 +0200

    Logger configurations

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const logger = require('./logger');
+const httpLogger = require('express-pino-logger')({logger: logger});
 const config = require('./config');
 const router = require('./router');
 
@@ -8,13 +9,14 @@ const app = express();
 
 app.use(express.json());
 //app.use(console.log);
-app.use(logger);
+app.use(httpLogger);
 app.use('/api/v1/', router);
 app.use((error, req, res, next) => {
     const statusCode = error.statusCode || 500;
+    logger.warn(error.toString);
     return res.status(statusCode).json({ error: error.toString() });
 });
 
-app.listen(config.port, () => console.log(`App is running on ${config.port}`));
+app.listen(config.port, () => logger.info(`App is running on ${config.port}`));
 
 module.exports = app;
--- logger.js
+++ logger.js
@@ -1,5 +1,11 @@
-const logger = require('express-pino-logger')({
-    prettyPrint: true 
+const config = require('./config');
+const logger = require('pino')({
+    prettyPrint: true,
+    //level: config.logger.level,
+    redact: {
+        paths: ['req.headers.cookie', 'req.headers.authorization', 'res.headers.cookie', 'res.headers.authorization'],
+        remove: true
+    }
 });
 
 module.exports = logger;
