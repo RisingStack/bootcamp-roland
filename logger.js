4a810a47bc984bfbe0fb862eea0d62ce641582a6
Kóczán Roland <roland.koczan@risingstack.com>
2020-10-08 11:51:27 +0200

    Uncommented logger level config

--- logger.js
+++ logger.js
@@ -1,7 +1,7 @@
 const config = require('./config');
 const logger = require('pino')({
     prettyPrint: true,
-    //level: config.logger.level,
+    level: config.logger.level,
     redact: {
         paths: ['req.headers.cookie', 'req.headers.authorization', 'res.headers.cookie', 'res.headers.authorization'],
         remove: true
