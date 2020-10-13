182c945d6f453ca44e6b26231c3921b31db6a40f
Kóczán Roland <roland.koczan@risingstack.com>
2020-10-13 13:13:45 +0200

    No longer passing username in middleware

--- authenticator.js
+++ authenticator.js
@@ -8,7 +8,6 @@ function authMiddleware(req, res, next) {
         if (!req.headers.authorization) throw 'Authentication failed!';
         const token = req.headers.authorization.replace(/Bearer\s*/, '');
         const decoded = jwt.verify(token, config.jwt);
-        req.body.username = decoded.username;
         next();
     } catch (error) {
         error.statusCode = 401;
