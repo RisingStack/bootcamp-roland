f49add3556b7fcc97f16b6eb02e717c79f9703cc
Kóczán Roland <roland.koczan@risingstack.com>
2020-10-14 11:05:26 +0200

    Added error message

--- authenticator.js
+++ authenticator.js
@@ -5,7 +5,7 @@ const config = require('./config');
 
 function authMiddleware(req, res, next) {
     try {
-        if (!req.headers.authorization) throw Error;
+        if (!req.headers.authorization) throw Error('Authentication failed');
         const token = req.headers.authorization.replace(/Bearer\s*/, '');
         jwt.verify(token, config.jwt);
         return next();
