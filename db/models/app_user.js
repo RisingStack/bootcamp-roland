478f564b49cb7e91a2d45fc681854808e5e1e68a
Kóczán Roland <roland.koczan@risingstack.com>
2020-10-14 09:24:02 +0200

    Removed parentheses

--- db/models/app_user.js
+++ db/models/app_user.js
@@ -1,8 +1,8 @@
 const db = require('../db');
 
-insert = (user) => (db('app_user').insert(user));
+insert = user => db('app_user').insert(user);
 
-read = async (username) => (db('app_user').where({username}).select('password'));
+read = username => db('app_user').where({username}).select('password');
 
 module.exports = {
     insert,
