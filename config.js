748d5b4c48d2985cf111b4e694cfb4882701c190
Kóczán Roland <roland.koczan@risingstack.com>
2020-10-28 15:29:24 +0100

    Added redis port and script

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ const config = {
   dbUser: process.env.NODE_ENV === 'test' ? 'postgres' : process.env.DBUSER,
   dbPassword: process.env.NODE_ENV === 'test' ? 'mysecretpassword' : process.env.DBPASSWORD,
   db: process.env.NODE_ENV === 'test' ? 'test' : process.env.DB,
+  redisPort: process.env.REDIS_PORT,
   logger: {
     level: process.env.LOG_LEVEL,
   },
