853c0a39360ec6cd0d5a6fc2cb687c2c984383ba
Kóczán Roland <roland.koczan@risingstack.com>
2020-11-02 07:49:09 +0100

    Contribution handler

--- worker/handlers/contribution.js
+++ worker/handlers/contribution.js
@@ -1,7 +1,37 @@
 const logger = require('../../logger');
+const userModel = require('../../db/models/user');
+const { getContributors } = require('../../services/github');
 
-function onContribution(query) {
-  logger.info(query);
+async function onContribution(message) {
+  const repositories = JSON.parse(message);
+
+  repositories.forEach(async repository => {
+    const repositoryName = repository.full_name;
+    logger.info(`repositoryName: ${repositoryName}`);
+    const dbResponse = await userModel.read({ id: repository.owner });
+    const owner = dbResponse[0];
+    logger.info(`userLogin: ${owner.login}`);
+
+    const githubResponse = await getContributors(owner.login, repositoryName);
+    const collaborators = githubResponse.repository.collaborators.edges;
+
+    logger.info(JSON.stringify(collaborators));
+
+    const usersToInsert = collaborators.map(async (collaborator) => {
+      const user = {
+        login: collaborator.node.login,
+        avatar_url: collaborator.node.avatarUrl,
+        html_url: collaborator.node.url,
+      };
+
+      const existingUser = await userModel.read(user);
+      if (!existingUser.length) {
+        return user;
+      }
+    });
+
+    Promise.all(usersToInsert).then((resolvedUsers) => userModel.insert(resolvedUsers));
+  });
 }
 
 module.exports = {
--- worker/handlers/repositroy.js
+++ worker/handlers/repositroy.js
@@ -2,46 +2,48 @@ const { searchRepositories } = require('../../services/github');
 const repositoryModel = require('../../db/models/repository');
 const userModel = require('../../db/models/user');
 
-async function onRepository(message) {
-  const { search } = await searchRepositories({ queryString: message });
+const onRepository = async (message) => (
+  new Promise(async (resolve, reject) => {
+    const { search } = await searchRepositories({ queryString: message, first: 1 });
 
-  const repositories = search.edges.map(async ({ node }) => {
-    const user = {
-      login: node.owner.login,
-      avatar_url: node.owner.avatarUrl,
-      html_url: node.owner.url,
-    };
+    const repositories = search.edges.map(async ({ node }) => {
+      const user = {
+        login: node.owner.login,
+        avatar_url: node.owner.avatarUrl,
+        html_url: node.owner.url,
+      };
 
-    const userResult = userModel.schema.validate(user);
-    if (userResult.error) throw Error(userResult.error);
+      const userResult = userModel.schema.validate(user);
+      if (userResult.error) throw Error(userResult.error);
 
-    let userToInsert = await userModel.read(user);
+      let userToInsert = await userModel.read(user);
 
-    if (!userToInsert.length) {
-      userToInsert = await userModel.insert(user);
-    }
+      if (!userToInsert.length) {
+        userToInsert = await userModel.insert(user);
+      }
 
-    const repository = {
-      owner: userToInsert[0].id,
-      full_name: node.name,
-      stargazers_count: node.stargazerCount,
-      html_url: node.homepageUrl,
-      description: node.description,
-      language: node.languages.edges[0].node.name,
-    };
+      const repository = {
+        owner: userToInsert[0].id,
+        full_name: node.name,
+        stargazers_count: node.stargazerCount,
+        html_url: node.homepageUrl,
+        description: node.description,
+        language: node.languages.edges[0].node.name,
+      };
 
-    const repositoryResult = repositoryModel.schema.validate(repository);
-    if (repositoryResult.error) throw Error(repositoryResult.error);
+      const repositoryResult = repositoryModel.schema.validate(repository);
+      if (repositoryResult.error) throw Error(repositoryResult.error);
 
-    return repository;
-  });
+      return repository;
+    });
 
-  Promise.all(repositories)
-    .then(repos => {
-      repositoryModel.insert(repos);
-      return repos;
-    }).catch(error => { throw Error(error); });
-}
+    Promise.all(repositories)
+      .then(resolvedRepos => {
+        repositoryModel.insert(resolvedRepos);
+        resolve(resolvedRepos);
+      }).catch(error => reject(error));
+  })
+);
 
 module.exports = {
   onRepository,
