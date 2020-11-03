c52fc3535ed3cecc8ac14adc8aa9b81c427e5ee8
Kóczán Roland <roland.koczan@risingstack.com>
2020-11-03 13:15:25 +0100

    Added redis config env vars

--- config.js
+++ config.js
@@ -8,7 +8,10 @@ const config = {
   dbUser: process.env.NODE_ENV === 'test' ? 'postgres' : process.env.DBUSER,
   dbPassword: process.env.NODE_ENV === 'test' ? 'mysecretpassword' : process.env.DBPASSWORD,
   db: process.env.NODE_ENV === 'test' ? 'test' : process.env.DB,
-  redisPort: process.env.REDIS_PORT,
+  redis: {
+    host: process.env.REDIS_PORT,
+    port: process.env.REDIS_HOST,
+  },
   logger: {
     level: process.env.LOG_LEVEL,
   },
--- worker/config.js
+++ worker/config.js
@@ -1,8 +1,8 @@
 const config = require('../config');
 
 const clientConfig = {
-  port: config.redisPort,
-  host: '172.17.0.1',
+  port: config.redis.port,
+  host: config.redis.host,
 };
 
 module.exports = clientConfig;
