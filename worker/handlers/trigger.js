abcf6f1c48bb54c0faf6dfa06aee57d7b8237074
Kóczán Roland <roland.koczan@risingstack.com>
2020-11-05 07:22:36 +0100

    Removed error handling, client creation, publish

--- worker/handlers/trigger.js
+++ worker/handlers/trigger.js
@@ -1,37 +1,13 @@
-const redis = require('redis');
-const Joi = require('joi');
-
-const redisConfig = require('../config');
 const logger = require('../../logger');
-const { channels } = require('../index');
+const { channels, triggerPublisher } = require('../index');
 
 function onTrigger(message) {
   if (!message) {
-    logger.info('Misisng query parameter!');
+    logger.error('Misisng query parameter!');
     process.exit(1);
   }
 
-  const client = redis.createClient(redisConfig);
-
-  client.pubsub('channels', (error, activeChannels) => {
-    if (error) {
-      logger.error(error);
-      process.exit(1);
-    }
-    try {
-      Joi.assert(activeChannels, Joi.array().items(
-        Joi.string().valid(channels.repository),
-        Joi.string().valid(channels.contribution),
-      ));
-    } catch (err) {
-      logger.error('Channels not subscribed!');
-      logger.error(err);
-      process.exit(1);
-    }
-  });
-
-  client.publish(channels.repository, message, () => logger.info(`Trigger message sent to ${channels.trigger} channel`));
-  client.quit();
+  triggerPublisher.publish(channels.repository, message, () => logger.info(`Trigger message sent to ${channels.trigger} channel`));
 }
 
 module.exports = {
