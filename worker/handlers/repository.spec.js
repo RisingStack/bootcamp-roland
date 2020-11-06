219c5a5845b073161ca3b2b67dddbc4d2dde852f
Kóczán Roland <roland.koczan@risingstack.com>
2020-11-06 07:42:52 +0100

    Added test cases

--- worker/handlers/repository.spec.js
+++ worker/handlers/repository.spec.js
@@ -1,20 +1,31 @@
 const chai = require('chai');
-const chaiHttp = require('chai-http');
 const sinon = require('sinon');
+const chaiAsPromised = require('chai-as-promised');
 
 const { onRepository } = require('./repository');
 const githubService = require('../../services/github');
+const repositoryModel = require('../../db/models/repository');
+
+chai.use(chaiAsPromised);
+const { expect } = chai;
+chai.should();
 
 const mockMessage = {
   search: {
     edges: [{
       node: {
-        owner: 'test',
+        owner: { login: 'testUser', avatarUrl: 'https://www.test.com', url: 'https://www.test.com' },
         name: 'testName',
         description: 'Test descreption',
         homepageUrl: 'test.com',
         stargazerCount: 10,
-        languages: 'JS',
+        languages: {
+          edges: [{
+            node: {
+              name: 'Test language',
+            },
+          }],
+        },
         createdAt: '2010',
       },
     }],
@@ -22,9 +33,40 @@ const mockMessage = {
 };
 
 describe('Repository worker handler', () => {
-  it('Should run without errors', async () => {
-    sinon.stub(githubService, 'searchRepositories').resolves(mockMessage);
+  let searchRepositories;
 
-    await onRepository('Test run');
+  beforeEach(function () {
+    searchRepositories = sinon.stub(githubService, 'searchRepositories');
+  });
+
+  afterEach(function () {
+    searchRepositories.restore();
+  });
+
+  it('Should run without errors', async () => {
+    searchRepositories.resolves(mockMessage);
+
+    const repository = await onRepository('Test run');
+
+    const response = await repositoryModel.read({ full_name: 'testName' });
+
+    const mockRepository = {
+      description: 'Test descreption',
+      full_name: 'testName',
+      html_url: 'test.com',
+      id: response[0].id,
+      language: 'Test language',
+      owner: 1,
+      stargazers_count: 10,
+    };
+
+    expect(repository).to.deep.equal(mockRepository);
+  });
+
+  it('Should be rejected with error', async () => {
+    const invalidResponse = null;
+    searchRepositories.resolves(invalidResponse);
+
+    onRepository('').should.be.rejectedWith(`Invalid Github API response from searchRepositories: ${invalidResponse}`);
   });
 });
